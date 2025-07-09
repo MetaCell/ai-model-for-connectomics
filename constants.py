62082121428516f78371459f35d124f716a3c1d9
Marcus Leiwe <marcus@metacell.us>
2025-07-09 21:18:06 +0800

    Extract synaptic coordinates from fafb via root id

---
+++ constants.py
@@ -0,0 +1,4 @@
+#########
+# This file contains constants scientific constants used throughout the project.
+#%% FAFB constants
+FAFB_VOXEL_RESOLUTION_NM = [4.0, 4.0, 40.0] # Voxel resolution in nanometers (x, y, z)
